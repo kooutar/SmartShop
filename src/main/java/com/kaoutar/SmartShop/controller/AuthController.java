8f015fcca9180b25bc45cf985d7595c6742eeac5
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-25 11:45:36 +0100

    feat : implimenter login

---
+++ src/main/java/com/kaoutar/SmartShop/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.kaoutar.SmartShop.controller;
+
+import com.kaoutar.SmartShop.DTO.LoginRequest;
+import com.kaoutar.SmartShop.model.User;
+import com.kaoutar.SmartShop.repositery.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final UserRepository userRepository;
+
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request, HttpSession session) {
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("Utilisateur non trouvé"));
+
+
+        if (!BCrypt.checkpw(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Mot de passe incorrect");
+        }
+
+        // Stocker l'utilisateur dans la session
+        session.setAttribute("user", user);
+
+        return "Connexion réussie";
+    }
+
+}
