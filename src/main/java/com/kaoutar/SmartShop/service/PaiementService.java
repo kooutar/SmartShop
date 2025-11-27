f56d1a96294af8c9f60747549ae2f44fdc5b8503
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-27 09:42:10 +0100

    fix (paiement) : fix le problème de null dans status et dateEncaissement

--- src/main/java/com/kaoutar/SmartShop/service/PaiementService.java
+++ src/main/java/com/kaoutar/SmartShop/service/PaiementService.java
@@ -2,14 +2,17 @@ package com.kaoutar.SmartShop.service;
 
 import com.kaoutar.SmartShop.DTO.PaiementDTO;
 import com.kaoutar.SmartShop.Mapper.PaiementMapper;
+import com.kaoutar.SmartShop.enums.PaymentStatus;
 import com.kaoutar.SmartShop.exception.BusinessException;
 import com.kaoutar.SmartShop.model.Commande;
 import com.kaoutar.SmartShop.model.Paiement;
 import com.kaoutar.SmartShop.repositery.CommandeRepository;
 import com.kaoutar.SmartShop.repositery.PaiementRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
 @AllArgsConstructor
 @Service
 public class PaiementService {
@@ -20,6 +23,15 @@ public class PaiementService {
 
     public PaiementDTO addPayement(Long orderId , PaiementDTO request){
         Commande Order = commandeRepository.findById(orderId).orElseThrow(()->new BusinessException("commande introuvable"));
+        switch (request.getTypePaiement()){
+            case "CHÈQUE" ->{
+                request.setStatut("EN_ATTENTE");
+            }
+            default -> {
+                request.setStatut("ENCAISSE");
+                request.setDateEncaissement(LocalDate.now());
+            }
+        }
         Paiement payment = paiementMapper.toEntity(request);
         payment.setCommande(Order);
         return  paiementMapper.toDto(paymentRepository.save(payment));
